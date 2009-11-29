750eea86617b8eb32d6c1f8d35b34a555068fd2c
Will Coleda <will@coleda.com>
2009-11-29 01:23:25 -0500

    add args handling to eval

--- src/PmTcl/Commands.pm
+++ src/PmTcl/Commands.pm
@@ -88,6 +88,9 @@ INIT {
 
 
 our sub eval(*@args) {
+    if +@args < 1 {
+        error('wrong # args: should be "eval arg ?arg ...?"');
+    }
     my $code := concat(|@args);
     our %EVALCACHE;
     my &sub := %EVALCACHE{$code};
