19094e7b51b5a8a902fa27fd578ac589e3714f4d
coke <will@coleda.com>
2009-12-19 18:32:56 +0100

    implement [lrepeat]

--- src/Partcl/commands/main.pm
+++ src/Partcl/commands/main.pm
@@ -352,6 +352,21 @@ our sub lrange(*@args) {
 }
 
 our sub lrepeat(*@args) {
+    if +@args < 2  {
+        error('wrong # args: should be "lrepeat positiveCount value ?value ...?"');
+    }
+    my $count := @args.shift.getInteger();
+    if $count < 1 {
+        error('must have a count of at least 1');
+    }
+    my @result := pir::new__ps('TclList');
+    while $count {
+        for @args -> $elem { 
+            @result.push($elem);
+        }
+        $count--;
+    }
+    return @result;
 }
 
 our sub lreplace(*@args) {
