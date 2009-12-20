5a6184db1cc0110f0bed3acf4a3abdda690e2ab6
coke <will@coleda.com>
2009-12-21 00:17:22 +0100

    add [linsert] - all tests but one pass.

--- src/Partcl/commands/main.pm
+++ src/Partcl/commands/main.pm
@@ -325,6 +325,23 @@ our sub lassign(*@args) {
 }
 
 our sub linsert(*@args) {
+    if +@args < 2 {
+        error('wrong # args: should be "linsert list index element ?element ...?"')
+    }
+    my @list := @args.shift().getList();
+    
+    #if user says 'end', make sure we use the end (imagine one element list)
+    my $oIndex := @args.shift();
+    my $index := @list.getIndex($oIndex);
+    if pir::substr__ssii($oIndex,0,3) eq 'end' {
+        $index++;
+    } else {
+        if $index > +@list { $index := +@list; }
+        if $index < 0      { $index := 0;}
+    }
+
+    pir::splice__vppii(@list, @args, $index, 0);
+    return @list;
 }
 
 our sub list(*@args) {
