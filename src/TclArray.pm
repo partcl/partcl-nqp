4a4c926f4f41da7d468c41c53b07aca4b883a5cd
Will "Coke" Coleda <will@coleda.com>
2012-09-05 19:00:39 -0400

    fixup Tcl{Array,LexPad,List,String}

--- src/TclArray.pm
+++ src/TclArray.pm
@@ -1,23 +1,23 @@
-#class TclArray {
-#    has @!array 
-#        is parrot_vtable_handler('get_pmc_keyed_int')
-#        is parrot_vtable_handler('set_pmc_keyed_int')
-#        is parrot_vtable_handler('set_pmc_keyed_str')
-#        is parrot_vtable_handler('exists_keyed_int')
-#        is parrot_vtable_handler('delete_keyed_int')
-#        is parrot_vtable_handler('unshift_pmc')
-#        is parrot_vtable_handler('push_pmc')
-#        ;
-#
-#    method new() {
-#        my $n := self.CREATE;
-#        $n.BUILD;
-#        $n
-#    }
-#
-#    method BUILD() {
-#        @!array := pir::new('Hash');
-#    }
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+class TclArray {
+    has @!array 
+        is parrot_vtable_handler('get_pmc_keyed_int')
+        is parrot_vtable_handler('set_pmc_keyed_int')
+        is parrot_vtable_handler('set_pmc_keyed_str')
+        is parrot_vtable_handler('exists_keyed_int')
+        is parrot_vtable_handler('delete_keyed_int')
+        is parrot_vtable_handler('unshift_pmc')
+        is parrot_vtable_handler('push_pmc')
+        ;
+
+    method new() {
+        my $n := self.CREATE;
+        $n.BUILD;
+        $n
+    }
+
+    method BUILD() {
+        @!array := pir::new__PS('Hash');
+    }
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
