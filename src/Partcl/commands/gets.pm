5acc2350851acded45137b9139eba3bf3aa416bd
Will "Coke" Coleda <will@coleda.com>
2012-09-05 14:03:53 -0400

    fixup [gets]

--- src/Partcl/commands/gets.pm
+++ src/Partcl/commands/gets.pm
@@ -1,26 +1,29 @@
-#sub gets(*@args) {
-#    our %CHANNELS;
-#
-#    if +@args < 1 || +@args > 2 {
-#        error('wrong # args: should be "gets channelId ?varName?"');
-#    }
-#
-#    my $channelId := @args[0];
-#    my $chanObj := %CHANNELS{$channelId};
-#    if (! pir::defined($chanObj) ) {
-#        error("can not find channel named \"$channelId\"");
-#    }
-#
-#    my $result := pir::readline__sp($chanObj);
-#    if pir::length($result) >0 && pir::substr__ssi($result, -1) eq "\n" {
-#        $result := pir::chopn__ssi($result,1);
-#    }
-#    if +@args == 2 {
-#        set(@args[1], $result);
-#        return pir::length($result);
-#    } else {
-#        return $result;
-#    }
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub gets(*@args) {
+    our %CHANNELS;
+
+    if +@args < 1 || +@args > 2 {
+        error('wrong # args: should be "gets channelId ?varName?"');
+    }
+
+    my $channelId := @args[0];
+    my $chanObj := %CHANNELS{$channelId};
+    if (! nqp::defined($chanObj) ) {
+        error("can not find channel named \"$channelId\"");
+    }
+
+
+    my $result := pir::readline__SP($chanObj);
+
+    if nqp::chars($result) >0 && nqp::substr($result, -1) eq "\n" {
+        $result := pir::chopn__SSi($result,1);
+    }
+    if +@args == 2 {
+        set(@args[1], $result);
+        return nqp::chars($result);
+    } else {
+        return $result;
+    }
+
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
