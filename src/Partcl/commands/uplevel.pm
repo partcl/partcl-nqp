65c22a6cc38df5e6465f4c421563be4cbeeb251a
Will "Coke" Coleda <will@coleda.com>
2012-09-05 14:06:10 -0400

    fixup [uplevel]

--- src/Partcl/commands/uplevel.pm
+++ src/Partcl/commands/uplevel.pm
@@ -1,21 +1,21 @@
-#sub uplevel($level, *@args) {
-#    ##  my %LEXPAD := DYNAMIC::<%LEXPAD>
-#    my %LEXPAD := pir::find_dynamic_lex('%LEXPAD');
-#
-#    ##  0x23 == '#'
-#    if pir::ord($level) == 0x23 {
-#        $level := %LEXPAD.depth - pir::substr($level, 1);
-#    }
-#
-#    ##  walk up the chain of outer contexts
-#    while $level > 0 {
-#        %LEXPAD := %LEXPAD.outer;
-#        $level := $level - 1;
-#    }
-#
-#    ##  now evaluate @args in the current context
-#    my $code := concat(|@args);
-#    Partcl::Compiler.eval($code);
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub uplevel($level, *@args) {
+    ##  my %LEXPAD := DYNAMIC::<%LEXPAD>
+    my %LEXPAD := pir::find_dynamic_lex__PS('%LEXPAD');
+
+    ##  0x23 == '#'
+    if nqp::ord($level) == 0x23 {
+        $level := %LEXPAD.depth - nqp::substr($level, 1);
+    }
+
+    ##  walk up the chain of outer contexts
+    while $level > 0 {
+        %LEXPAD := %LEXPAD.outer;
+        $level := $level - 1;
+    }
+
+    ##  now evaluate @args in the current context
+    my $code := concat(|@args);
+    Partcl::Compiler.eval($code);
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
