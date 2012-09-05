9ee17d664d84f8eca2f67330749aeae4e17a8ab0
Will "Coke" Coleda <will@coleda.com>
2012-09-05 14:19:49 -0400

    fixup [lrange]

--- src/Partcl/commands/lrange.pm
+++ src/Partcl/commands/lrange.pm
@@ -1,21 +1,21 @@
-#sub lrange(*@args) {
-#    if +@args != 3 {
-#        error('wrong # args: should be "lrange list first last"')
-#    }
-#    my @list := @args[0].getList();
-#    my $from := @list.getIndex(@args[1]);
-#    my $to   := @list.getIndex(@args[2]);
-#
-#    if $from < 0 { $from := 0}
-#    my $listLen := +@list;
-#    if $to > $listLen { $to := $listLen - 1 }
-#
-#    my @retval := pir::new('TclList');
-#    while $from <= $to  {
-#        @retval.push(@list[$from]);
-#        $from++;
-#    }
-#    return @retval;
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub lrange(*@args) {
+    if +@args != 3 {
+        error('wrong # args: should be "lrange list first last"')
+    }
+    my @list := @args[0].getList();
+    my $from := @list.getIndex(@args[1]);
+    my $to   := @list.getIndex(@args[2]);
+
+    if $from < 0 { $from := 0}
+    my $listLen := +@list;
+    if $to > $listLen { $to := $listLen - 1 }
+
+    my @retval := pir::new__PS('TclList');
+    while $from <= $to  {
+        @retval.push(@list[$from]);
+        $from++;
+    }
+    return @retval;
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
