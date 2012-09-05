0101c080f588b7b8267ce52607a380ceb02f564f
Will "Coke" Coleda <will@coleda.com>
2012-09-05 10:29:01 -0400

    fixup [exit]

--- src/Partcl/commands/exit.pm
+++ src/Partcl/commands/exit.pm
@@ -1,9 +1,9 @@
-#sub exit(*@args) {
-#    if +@args > 1 {
-#        error('wrong # args: should be "exit ?returnCode?"');
-#    }
-#    my $code := @args[0] // 0;
-#    pir::exit($code);
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub exit(*@args) {
+    if +@args > 1 {
+        error('wrong # args: should be "exit ?returnCode?"');
+    }
+    my $code := @args[0] // 0;
+    nqp::exit($code);
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
