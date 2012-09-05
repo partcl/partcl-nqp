c302650846d553b57539c3c033bed51ce998c1c7
Will "Coke" Coleda <will@coleda.com>
2012-09-05 10:38:44 -0400

    fixup [lappend]

--- src/Partcl/commands/lappend.pm
+++ src/Partcl/commands/lappend.pm
@@ -1,22 +1,22 @@
-#sub lappend(*@args) {
-#    if +@args < 1 {
-#        error('wrong # args: should be "lappend varName ?value value ...?"');
-#    }
-#    my $var := @args.shift();
-#    my @list;
-#    # lappend auto-vivifies
-#    try {
-#        @list := set($var);
-#        CATCH {
-#            @list := set($var, pir::new('TclList'));
-#        }
-#    }
-#    @list := @list.getList();
-#
-#    for @args -> $elem {
-#        @list.push($elem);
-#    }
-#    return set($var,@list);
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub lappend(*@args) {
+    if +@args < 1 {
+        error('wrong # args: should be "lappend varName ?value value ...?"');
+    }
+    my $var := @args.shift();
+    my @list;
+    # lappend auto-vivifies
+    try {
+        @list := set($var);
+        CATCH {
+            @list := set($var, pir::new__Ps('TclList'));
+        }
+    }
+    @list := @list.getList();
+
+    for @args -> $elem {
+        @list.push($elem);
+    }
+    return set($var,@list);
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
