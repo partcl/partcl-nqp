c5da276eb53825fd03c8ff5d44a7c19f7314f479
Will "Coke" Coleda <will@coleda.com>
2012-09-05 14:07:33 -0400

    fixup [flush]

--- src/Partcl/commands/flush.pm
+++ src/Partcl/commands/flush.pm
@@ -1,12 +1,12 @@
-#sub flush(*@args) {
-#    if +@args != 1 {
-#        error('wrong # args: should be "flush channelId"');
-#    }
-#    my $ioObj := _getChannel(@args[0]);
-#    if pir::can($ioObj, 'flush') {
-#        $ioObj.flush();
-#    }
-#    '';
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub flush(*@args) {
+    if +@args != 1 {
+        error('wrong # args: should be "flush channelId"');
+    }
+    my $ioObj := _getChannel(@args[0]);
+    if nqp::can($ioObj, 'flush') {
+        $ioObj.flush();
+    }
+    '';
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
