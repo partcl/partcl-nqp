7acaa5b24b8b94516cc7b87eb83fdccb3d67604d
Will "Coke" Coleda <will@coleda.com>
2012-09-05 10:33:43 -0400

    fixup [lrepeat]

--- src/Partcl/commands/lrepeat.pm
+++ src/Partcl/commands/lrepeat.pm
@@ -1,19 +1,19 @@
-#sub lrepeat(*@args) {
-#    if +@args < 2  {
-#        error('wrong # args: should be "lrepeat positiveCount value ?value ...?"');
-#    }
-#    my $count := @args.shift.getInteger();
-#    if $count < 1 {
-#        error('must have a count of at least 1');
-#    }
-#    my @result := pir::new('TclList');
-#    while $count {
-#        for @args -> $elem {
-#            @result.push($elem);
-#        }
-#        $count--;
-#    }
-#    return @result;
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub lrepeat(*@args) {
+    if +@args < 2  {
+        error('wrong # args: should be "lrepeat positiveCount value ?value ...?"');
+    }
+    my $count := @args.shift.getInteger();
+    if $count < 1 {
+        error('must have a count of at least 1');
+    }
+    my @result := pir::new__Ps('TclList');
+    while $count {
+        for @args -> $elem {
+            @result.push($elem);
+        }
+        $count--;
+    }
+    return @result;
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
