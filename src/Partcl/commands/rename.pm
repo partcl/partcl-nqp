ed3a891d43935d8223d83aad153066ba86880ed8
Will "Coke" Coleda <will@coleda.com>
2012-09-07 10:33:09 -0400

    fixup [rename]
    moritz++

--- src/Partcl/commands/rename.pm
+++ src/Partcl/commands/rename.pm
@@ -1,12 +1,12 @@
-#sub rename(*@args) {
-#    if +@args != 2 {
-#        error('wrong # args: should be "rename oldName newName"');
-#    }
-#    my $hll := pir::get_hll_namespace__P();
-#    if @args[1] ne "" {
-#        $hll{~@args[1]} := $hll{~@args[0]};
-#    }
-#    pir::delete($hll, @args[0]);
-#}
-#
-## vim: expandtab shiftwidth=4 ft=perl6:
+sub rename(*@args) {
+    if +@args != 2 {
+        error('wrong # args: should be "rename oldName newName"');
+   }
+    my $hll := pir::get_hll_namespace__P();
+    if @args[1] ne "" {
+        $hll{~@args[1]} := $hll{~@args[0]};
+    }
+    nqp::deletekey($hll, @args[1]);
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6:
