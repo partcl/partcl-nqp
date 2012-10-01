2eb8bf337aef07411c2e7f85174e37ecadd77a67
Will "Coke" Coleda <will@coleda.com>
2012-09-30 21:20:48 -0400

    Use SVals, not strings.
    With this, "puts hi" now works again

--- src/Partcl/Actions.pm
+++ src/Partcl/Actions.pm
@@ -1,22 +1,26 @@
 use src::Internals;
 
 class Partcl::Actions is HLL::Actions {
-
-    method TOP($/) { make $<TOP_eval>.ast; }
+    method TOP($/) { 
+        make $<TOP_eval>.ast;
+    }
     
     ## TOP_eval and TOP_expr create a PAST::Block that uses the
     ## lexical scope given by the caller's %LEXPAD.
     
-    method TOP_eval($/) { make eval_block($<body>.ast); }
-    method TOP_expr($/) { make eval_block($<EXPR>.ast); }
-    
+    method TOP_eval($/) {
+        make eval_block($<body>.ast);
+    }
+    method TOP_expr($/) {
+        make eval_block($<EXPR>.ast);
+    }
+
     sub eval_block($past) {
         ## This is the runtime equivalent of
         ##     register lexpad := DYNAMIC::<%LEXPAD>;
         ## The body of the code to be evaluated
         my $lexpad_init :=
             QAST::Var.new( :name<lexpad>, :scope<local>, :decl<var>);
-  
   
         my $block;  
         if ! nqp::isnull(pir::find_dynamic_lex__PS('@*PARTCL_COMPILER_NAMESPACE')) {
@@ -32,7 +36,9 @@ class Partcl::Actions is HLL::Actions {
     ## TOP_proc creates a PAST::Block that initializes a
     ## new lexical scope in %LEXPAD.
     
-    method TOP_proc($/) { make lex_block($<body>.ast); }
+    method TOP_proc($/) {
+        make lex_block($<body>.ast);
+    }
     
     sub lex_block($past) {
         ## This is the runtime equivalent of
@@ -50,7 +56,9 @@ class Partcl::Actions is HLL::Actions {
         }
     }
     
-    method body($/) { make $<script>.ast; }
+    method body($/) {
+        make $<script>.ast;
+    }
     
     method script($/) {
         my $past := QAST::Stmts.new( :node($/) );
@@ -65,66 +73,139 @@ class Partcl::Actions is HLL::Actions {
         my $past := QAST::Op.new( 
                :op('callmethod'), :name('dispatch'), :node($/),
                QAST::WVal.new( :value(Internals) ) );
-$past.push( QAST::SVal.new(:value("puts")));
-$past.push( QAST::SVal.new(:value("hi")));
-        #my $i := 0;
-        #my $n := +$<word>;
-        #while $i < $n {
-            #$past.push($<word>[$i].ast);
-            #$i++;
-        #}
+        my $i := 0;
+        my $n := +$<word>;
+        while $i < $n {
+            my $ast := $<word>[$i].ast;
+            $past.push($ast);
+            $i++;
+        }
         make $past;
     }
     
-    method word:sym<{*}>($/)  {
+    method word:sym<{*}>($/) {
         make QAST::Op.new( :op<call>, :name<EXPAND>, $<word>.ast, :flat);
     }
-    method word:sym<{ }>($/)  { make $<braced_word>.ast; }
-    method word:sym<" ">($/)  { make $<quoted_word>.ast; }
-    method word:sym<bare>($/) { make concat_atoms($<bare_atom>); }
+    method word:sym<{ }>($/) {
+        make $<braced_word>.ast;
+    }
+    method word:sym<" ">($/) {
+        make $<quoted_word>.ast;
+    }
+    method word:sym<bare>($/) {
+        make concat_atoms($<bare_atom>);
+    }
     
-    method braced_word($/) { make concat_atoms($<braced_atom>); }
-    method braced_atom:sym<{ }>($/)    { make '{' ~ $<braced_word>.ast ~ '}'; }
-    method braced_atom:sym<backnl>($/) { make ' '; }
-    method braced_atom:sym<back{>($/)  { make "\\" ~ '{'; }
-    method braced_atom:sym<back}>($/)  { make "\\" ~ '}'; }
-    method braced_atom:sym<backd>($/)  { make "\\" ~ "\\"; }
-    method braced_atom:sym<back>($/)   { make "\\"; }
-    method braced_atom:sym<chr>($/)    { make ~$/; }
+    method braced_word($/) {
+        make concat_atoms($<braced_atom>);
+    }
+    method braced_atom:sym<{ }>($/) {
+        make QAST::SVal.new(:value(
+            '{' ~ $<braced_word>.ast ~ '}'
+        ));
+    }
+    method braced_atom:sym<backnl>($/) {
+        make QAST::SVal.new(:value(' '));
+    }
+    method braced_atom:sym<back{>($/) {
+        make QAST::SVal.new(:value("\\" ~ '{'));
+    }
+    method braced_atom:sym<back}>($/) {
+        make QAST::SVal.new(:value("\\" ~ '}'));
+    }
+    method braced_atom:sym<backd>($/) {
+        make QAST::SVal.new(:value("\\" ~ "\\"));
+    }
+    method braced_atom:sym<back>($/) {
+        make QAST::SVal.new(:value("\\"));
+    }
+    method braced_atom:sym<chr>($/) {
+        make QAST::SVal.new(:value(~$/));
+    }
     
-    method quoted_word($/) { make concat_atoms($<quoted_atom>); }
+    method quoted_word($/) {
+        make concat_atoms($<quoted_atom>);
+    }
     
-    method quoted_atom:sym<[ ]>($/) { make $<script>.ast; }
-    method quoted_atom:sym<var>($/) { make $<variable>.ast; }
-    method quoted_atom:sym<$>($/)   { make '$'; }
-    method quoted_atom:sym<\\>($/)  { make $<backslash>.ast; }
-    method quoted_atom:sym<chr>($/) { make ~$/; }
+    method quoted_atom:sym<[ ]>($/) {
+        make $<script>.ast;
+    }
+    method quoted_atom:sym<var>($/) {
+        make $<variable>.ast;
+    }
+    method quoted_atom:sym<$>($/) {
+        make QAST::SVal.new(:value('$'));
+    }
+    method quoted_atom:sym<\\>($/) {
+        make $<backslash>.ast;
+    }
+    method quoted_atom:sym<chr>($/) {
+        make QAST::SVal.new(:value(~$/));
+    }
     
-    method bare_atom:sym<[ ]>($/) { make $<script>.ast; }
-    method bare_atom:sym<var>($/) { make $<variable>.ast; }
-    method bare_atom:sym<$>($/)   { make '$'; }
-    method bare_atom:sym<\\>($/)  { make $<backslash>.ast; }
-    method bare_atom:sym<chr>($/) { make ~$/; }
+    method bare_atom:sym<[ ]>($/) {
+        make $<script>.ast;
+    }
+    method bare_atom:sym<var>($/) {
+        make $<variable>.ast;
+    }
+    method bare_atom:sym<$>($/) {
+        make QAST::SVal.new(:value('$'));
+    }
+    method bare_atom:sym<\\>($/) {
+        make $<backslash>.ast;
+    }
+    method bare_atom:sym<chr>($/) {
+        make QAST::SVal.new(:value(~$/));
+    }
     
-    method backslash:sym<bel>($/)   { make "\x07"; }
-    method backslash:sym<bs>($/)    { make "\x08"; }
-    method backslash:sym<ff>($/)    { make "\x0c"; }
-    method backslash:sym<lf>($/)    { make "\x0a"; }
-    method backslash:sym<cr>($/)    { make "\x0d"; }
-    method backslash:sym<ht>($/)    { make "\x09"; }
-    method backslash:sym<vt>($/)    { make "\x0b"; }
-    method backslash:sym<chr>($/)   { make ~$<chr>; }
-    method backslash:sym<backnl>($/) { make ' '; }
+    method backslash:sym<bel>($/) {
+        make QAST::SVal.new(:value("\x07"));
+    }
+    method backslash:sym<bs>($/) { 
+        make QAST::SVal.new(:value("\x08"));
+    }
+    method backslash:sym<ff>($/) {
+        make QAST::SVal.new(:value("\x0c"));
+    }
+    method backslash:sym<lf>($/) {
+        make QAST::SVal.new(:value("\x0a"));
+    }
+    method backslash:sym<cr>($/) {
+        make QAST::SVal.new(:value("\x0d"));
+    }
+    method backslash:sym<ht>($/) {
+        make QAST::SVal.new(:value("\x09"));
+    }
+    method backslash:sym<vt>($/) {
+        make QAST::SVal.new(:value("\x0b"));
+    }
+    method backslash:sym<chr>($/) {
+        make QAST::SVal.new(:value(~$<chr>));
+    }
+    method backslash:sym<backnl>($/) {
+        make QAST::SVal.new(:value(" "));
+    }
     method backslash:sym<backx>($/) {
         my $len := nqp::chars(~$<x>);
         my $substr_len := ($len >= 2) ?? -2 !! -$len;
-        make nqp::chr(HLL::Actions::string_to_int(nqp::substr(~$<x>, $substr_len), 16));
+        make QAST::SVal.new(:value(
+            nqp::chr(
+                HLL::Actions::string_to_int(
+                    nqp::substr(~$<x>, $substr_len), 16
+                )
+            )
+        ));
     }
     method backslash:sym<backo>($/) {
-        make nqp::chr(HLL::Actions::string_to_int(~$<o>, 8));
+        make QAST::SVal.new(:value(
+            nqp::chr(HLL::Actions::string_to_int(~$<o>, 8))
+        ));
     }
     method backslash:sym<backu>($/) {
-        make nqp::chr(HLL::Actions::string_to_int(~$<u>, 16));
+        make QAST::SVal.new(:value(
+            nqp::chr(HLL::Actions::string_to_int(~$<u>, 16))
+        ));
     }
     
     method list($/) {
@@ -136,9 +217,15 @@ $past.push( QAST::SVal.new(:value("hi")));
     
         make @list;
     }
-    method list_word($/) { make concat_atoms($<list_atom>); }
-    method list_atom:sym<\\>($/)  { make $<backslash>.ast; }
-    method list_atom:sym<chr>($/) { make ~$/; }
+    method list_word($/) {
+        make concat_atoms($<list_atom>);
+    }
+    method list_atom:sym<\\>($/) {
+        make $<backslash>.ast;
+    }
+    method list_atom:sym<chr>($/) {
+        make QAST::SVal.new(:value(~$/));
+    }
     
     sub concat_atoms(@atoms) {
         my @parts;
@@ -222,10 +309,10 @@ $past.push( QAST::SVal.new(:value("hi")));
     
     method variable:sym<escaped>($/) {
         make QAST::Var.new( :scope<keyed>,
-                 QAST::Var.new( :name<lexpad>, :scope<register> ),
-                 ~$<identifier>,
-                 :node($/)
-             );
+            QAST::Var.new( :name<lexpad>, :scope<register> ),
+            ~$<identifier>,
+            :node($/)
+        );
     }
     
     method integer($/) {
@@ -236,20 +323,40 @@ $past.push( QAST::SVal.new(:value("hi")));
         }
     }
     
-    method int:sym<oct>($/) { make HLL::Actions::string_to_int(~$<digits>, 8) }
-    method int:sym<dec>($/) { make HLL::Actions::string_to_int(~$<digits>, 10) }
-    method int:sym<hex>($/) { make HLL::Actions::string_to_int(~$<digits>, 16) }
+    method int:sym<oct>($/) {
+        make HLL::Actions::string_to_int(~$<digits>, 8)
+    }
+    method int:sym<dec>($/) {
+        make HLL::Actions::string_to_int(~$<digits>, 10)
+    }
+    method int:sym<hex>($/) {
+        make HLL::Actions::string_to_int(~$<digits>, 16)
+    }
     
-    method term:sym<true>($/)  { make $/.Str }
-    method term:sym<false>($/) { make $/.Str }
+    method term:sym<true>($/) {
+        make $/.Str
+    }
+    method term:sym<false>($/) {
+        make $/.Str
+    }
     
-    method term:sym<variable>($/) { make $<variable>.ast; }
-    method term:sym<integer>($/) { make $<integer>.ast; }
+    method term:sym<variable>($/) {
+        make $<variable>.ast;
+    }
+    method term:sym<integer>($/) {
+        make $<integer>.ast;
+    }
     
-    method term:sym<( )>($/) { make $<EXPR>.ast; }
+    method term:sym<( )>($/) {
+        make $<EXPR>.ast;
+    }
     
-    method term:sym<[ ]>($/) { make $<script>.ast; }
-    method term:sym<" ">($/) { make concat_atoms($<quoted_atom>); }
+    method term:sym<[ ]>($/) {
+        make $<script>.ast;
+    }
+    method term:sym<" ">($/) {
+        make concat_atoms($<quoted_atom>);
+    }
 
 =begin head1 index
     
