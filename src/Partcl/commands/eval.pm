496b9d1f6fbad7596038757db9b0d0f58884b2f4
Will "Coke" Coleda <will@coleda.com>
2012-10-30 21:13:15 -0400

    fix [eval puts hi]

--- src/Partcl/commands/eval.pm
+++ src/Partcl/commands/eval.pm
@@ -2,9 +2,10 @@ method eval(*@args) {
     if +@args < 1 {
         self.error('wrong # args: should be "eval arg ?arg ...?"');
     }
-    my $code := concat(|@args);
-    my &sub := Partcl::Compiler.compile($code);
-    &sub();
+    my $code := Builtins.new.concat(|@args);
+    my $tcl := pir::compreg__PS('Partcl');
+    my $sub := $tcl.compile($code);
+    $sub();
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6:
