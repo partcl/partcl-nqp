e552619bf0f023704f39a5cf37642540e7db36cd
Will "Coke" Coleda <will@coleda.com>
2013-06-13 19:05:48 -0400

    uncomment out file, make it compile

--- src/Partcl/Operators.pm
+++ src/Partcl/Operators.pm
@@ -1,9 +1,7 @@
-=begin operators
-
 sub &infix:<==>($a, $b) {
     # Try to do a numeric compare first (XXX this is integer for now)
     try {
-        return pir::set__iP($a) == pir::set__iP($b);
+        return +$a == +$b;
         CATCH {
             # Not numeric. Try as string...
             return $a eq $b;
@@ -29,7 +27,5 @@ sub &infix:<in>($check, @list) {
    }
    return 0;
 }
-
-=end operators
 
 # vim: expandtab shiftwidth=4 ft=perl6:
