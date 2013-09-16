96f655d68acf5127e7d2ad9681b40356ce44310a
Will "Coke" Coleda <will@coleda.com>
2013-09-16 10:45:41 -0400

    Add a simplistic runner script

--- src/Partcl.pm
+++ src/Partcl.pm
@@ -1,7 +1,7 @@
 use NQPHLL;
 
 use src::init;
-#use src::Partcl::commands;
+use src::Partcl::commands;
 use src::Partcl::Operators;
 use src::TclArray;
 use src::TclLexPad;
@@ -19,11 +19,15 @@ use src::FileGlob::Compiler;
 use src::options;
 
 ## XXX JVM/Parrot differ on how to define MAIN
-sub MAIN(*@ARGS) {
+sub MAIN($partcl, $filename) {
+    # The first argument is the pm file for partcl
+    my @ARGS := [];
+    nqp::push(@ARGS, $filename);
     my %LEXPAD;
     my $compiler := Partcl::Compiler.new();
     $compiler.language('Partcl');
     $compiler.parsegrammar(Partcl::Grammar);
     $compiler.parseactions(Partcl::Actions);
+    # TODO _ why is @ARGS not being processed?
     $compiler.command_line(@ARGS, :encoding('utf8'), :transcode('ascii iso-8859-1'));
 }
